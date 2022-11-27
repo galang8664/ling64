a26efe41452793262f325fc5ecbc9eed34e151f4
galang8664 <95033122+galang8664@users.noreply.github.com>
2022-11-27 19:44:51 +0700

    lava

--- build_rom.sh
+++ build_rom.sh
@@ -4,13 +4,13 @@ set -e
 set -x
 
 # sync rom
-repo init --depth=1 -u git://github.com/AospExtended/manifest.git -b 11.x
-git clone https://github.com/Apon77Lab/android_.repo_local_manifests.git --depth 1 -b aex .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
+repo init -u https://github.com/crdroidandroid/android.git -b 13.0
+git clone https://gitlab.com/R9Lab/Manifest.git --depth 1 -b LineageOS-13 .repo/local_manifests
+repo sync
 
 # build rom
-source build/envsetup.sh
-lunch aosp_mido-user
+. build/envsetup.sh
+brunch lava
 m aex -j$(nproc --all)
 
 # upload rom
